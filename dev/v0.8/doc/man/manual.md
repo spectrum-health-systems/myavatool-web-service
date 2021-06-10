96c942eeb9296138abd9f88eb228d5d0600cdf11
APrettyCoolProgram <development@aprettycoolprogram.com>
2021-06-10 13:37:24 -0400

    Development resource update

---
+++ dev/v0.8/doc/man/manual.md
@@ -0,0 +1,62 @@
+﻿<!--
+  Software manual template (b210104)
+  https://github.com/APrettyCoolProgram/my-development-environment/tree/master/templates/documentation
+-->
+
+<h1 align="center">
+
+  <img src="../../resources/asset/img/logo/maws-logo-800x150.png" alt="myAvatar Web Service logo" width="800">
+  <br>
+  MANUAL
+  <br>
+
+</h1>
+
+<h6 align="center">
+
+  MAWS v0.8.x&nbsp;&bull;&nbsp;Last updated: April 21st, 2021
+
+</h6>
+
+***
+
+<h5 align="center">
+
+  HOME&nbsp;&bull;&nbsp;[GETTING STARTED](manual-getting-started.md)&nbsp;&bull;&nbsp;[HOSTING MAWS](manual-hosting-maws.md)&nbsp;&bull;&nbsp;[IMPORTING MAWS](manual-importing-maws.md)&nbsp;&bull;&nbsp;[USING MAWS](manual-using-maws.md)&nbsp;&bull;&nbsp;[SCRIPTLINK EVENTS](manual-scriptlink-events.md)&nbsp;&bull;&nbsp;[ADDITIONAL INFORMATION](manual-additional-information.md)
+
+</h5>
+
+***
+
+<h6 align="center">
+
+  <img src="https://img.shields.io/badge/WARNING:-THIS%20IS%20BETA%20SOFTWARE-%23990000?style=for-the-badge">
+  
+</h6>
+
+# THE MYAVATOOL WEB SERVICE (MAWS)
+[Netsmart's myAvatar™](https://www.ntst.com/Solutions-and-Services/Offerings/myAvatar) is a behavioral health EHR that offers a recovery-focused suite of solutions that leverage real-time analytics and clinical decision support to drive value-based care.
+
+While myAvatar™ is a robust platform, like most things in life (except [Heroes of Might and Magic III](https://www.gog.com/game/heroes_of_might_and_magic_3_complete_edition)), it isn't perfect.
+
+The good news is that myAvatar™ functionality can be extended via Netsmart's myAvatar™ Web Services, and/or custom web services that are written by myAvatar™ users.
+
+The myAvatool Web Service (**MAWS**) is one such custom web service which includes various tools and utilities for myAvatar™ that aren't included in the official release, and provides a solid foundation for building additional functionality quickly and efficiently.
+
+## MAWS FEATURES
+* Several built-in tools and utilities for use with myAvatar™
+* Does not require Java to be installed
+* A solid foundation to build additional myAvatar™ custom tools and utilities
+
+## THE MANUAL
+The MAWS Manual (which you are reading *right now*) is updated with each release, and will walk you through setting up and using MAWS in your environments.
+
+## GETTING STARTED
+Ready to setup MAWS in your environment? [Let's get started!](manual-getting-started.md)
+***
+
+<h5 align="center">
+
+  HOME&nbsp;&bull;&nbsp;[GETTING STARTED](manual-getting-started.md)&nbsp;&bull;&nbsp;[HOSTING MAWS](manual-hosting-maws.md)&nbsp;&bull;&nbsp;[IMPORTING MAWS](manual-importing-maws.md)&nbsp;&bull;&nbsp;[USING MAWS](manual-using-maws.md)&nbsp;&bull;&nbsp;[SCRIPTLINK EVENTS](manual-scriptlink-events.md)&nbsp;&bull;&nbsp;[ADDITIONAL INFORMATION](manual-additional-information.md)
+
+</h5>
---
+++ dev/v0.8/src/MyAvatoolWebService.asmx.cs
@@ -0,0 +1,105 @@
+﻿/* PROJECT: MyAvatoolWebService (https://github.com/spectrum-health-systems/MyAvatoolWebService)
+ *    FILE: MyAvatoolWebService.MyAvatoolWebService.asmx.cs
+ * UPDATED: 4-21-2021-11:35 AM
+ * LICENSE: Apache v2 (https://apache.org/licenses/LICENSE-2.0)
+ *          Copyright 2021 A Pretty Cool Program, All Rights reserved
+ */
+
+/**********************************************************************************************************************************
+ *                                 >>> WARNING: THIS IS THE MAWS DEVELOPMENT BRANCH <<<                                           *
+ **********************************************************************************************************************************
+ * Unless I have forgotten to update this comment, you are looking at v0.8 of the MAWS development branch. You can confirm this   *
+ * by checking the following line in /Properties/AssemblyInfo.cs:                                                                 *
+ *                                                                                                                                *
+ *  [assembly: AssemblyVersion("X.X.X.X")]                                                                                        *
+ *                                                                                                                                *
+ * To make sure you are using the latest development branch version, check the /Resources/Dev/current-versions.md file:           *
+ *  https://github.com/spectrum-health-systems/MyAvatoolWebService/blob/development/src/Resources/Dev/development-information.md  *
+ *                                                                                                                                *
+ * The MAWS development branch should not be used in production environments.                                                     *
+ *                                                                                                                                *
+ * For previous development branch versions, please see:                                                                          *
+ *  https://github.com/spectrum-health-systems/MyAvatoolWebService/tree/main/dev                                                  *
+ *********************************************************************************************************************************/
+
+/* For information about this sourcecode, please see:
+ *  https://github.com/spectrum-health-systems/MyAvatoolWebService/blob/development/src/Resources/Dev/sourcecode-information.md
+ */
+
+using System.Web.Services;
+using NTST.ScriptLinkService.Objects;
+
+namespace MyAvatoolWebService
+{
+    /// <summary>
+    /// Summary description for MyAvatoolWebService.
+    /// </summary>
+    /// <remarks>
+    /// DO NOT REMOVE ANY OF THIS CODE. All code in this class is required by myAvatar.
+    /// </remarks>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    public class MyAvatoolWebService : WebService
+    {
+        /// <summary>
+        /// Returns the MAWS version string.
+        /// </summary>
+        /// <returns>The MAWS version string (e.g., "VERSION 1.0").</returns>
+        /// <remarks>
+        /// * DO NOT REMOVE THIS METHOD. It is required by myAvatar.
+        /// * For detailed information about GetVersion(), please see the MAWS manual:
+        ///     https://github.com/spectrum-health-systems/MyAvatoolWebService/blob/main/doc/man/manual.md
+        /// </remarks>
+        [WebMethod]
+        public string GetVersion()
+        {
+            return "VERSION 1.0";
+        }
+
+        /// <summary>
+        /// Performs an MAWS request
+        /// </summary>
+        /// <param name="sentOptionObject2">The OptionObject2 object sent from myAvatar.</param>
+        /// <param name="mawsRequest">      The MAWS request to perform (e.g., "InptAdmitDate-VerifyPreAdmin")</param>
+        /// <returns>A completed OptionObject2 that MAWS will return to myAvatar.</returns>
+        /// <remarks>
+        /// * DO NOT REMOVE THIS METHOD. It is required by myAvatar.
+        /// * For detailed information about RunScript(), please see the MAWS manual:
+        ///     https://github.com/spectrum-health-systems/MyAvatoolWebService/blob/main/doc/man/manual.md
+        /// </remarks>
+        [WebMethod]
+        public OptionObject2 RunScript(OptionObject2 sentOptionObject2, string mawsRequest)
+        {
+            var workingOptionObject2 = new OptionObject2();
+
+            /* Let's have a quick description of this code block here, and more in-depth in the manual.
+             */
+            if(mawsRequest.Contains("InptAdmitDate"))
+            {
+                workingOptionObject2 = InptAdmitDate.Parser(sentOptionObject2, mawsRequest);
+            }
+            else if(mawsRequest.Contains("SubPolicyNumber"))
+            {
+                //completedOptionObject2 = SubPolicyNumber(sentOptionObject2, mawsRequest);
+            }
+            else if(mawsRequest.Contains("NewWebServiceCommand-01"))
+            {
+                //completedOptionObject2 = NewWebServiceCommand-02(sentOptionObject2, mawsRequest);
+            }
+            else if(mawsRequest.Contains("NewWebServiceCommand-02"))
+            {
+                //completedOptionObject2 = NewWebServiceCommand-02(sentOptionObject2, mawsRequest);
+            }
+            else
+            {
+                workingOptionObject2 = sentOptionObject2;
+            }
+
+            // TODO completed OO2 stuff here?
+            OptionObject2 completedOptionObject2 = OptionObjectMaintenance.Complete(sentOptionObject2, workingOptionObject2, true,false );
+
+            return completedOptionObject2;
+        }
+    }
+}
